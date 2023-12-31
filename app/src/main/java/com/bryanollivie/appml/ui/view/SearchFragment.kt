c84ef867d189e88ee34f25d7309dee7fd621d150
bryanollivie <bryanollivie@gmail.com>
2023-12-31 14:17:24 -0300

    Layout screen search done

--- app/src/main/java/com/bryanollivie/appml/ui/view/SearchFragment.kt
+++ app/src/main/java/com/bryanollivie/appml/ui/view/SearchFragment.kt
@@ -1,15 +1,18 @@
 package com.bryanollivie.appml.ui.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.bryanollivie.appml.R
 import com.bryanollivie.appml.databinding.FragmentSearchBinding
 import com.bryanollivie.appml.ui.viewmodel.SharedViewModel
+import com.bryanollivie.appml.util.AppUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,9 +20,7 @@ class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
-
     private val sharedViewModel: SharedViewModel by activityViewModels()
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,11 +33,23 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        view.setOnTouchListener { _, _ ->
+            AppUtils.hideKeyboard(requireContext(),binding.editTextSearch)
+            false
+        }
+
         binding.buttonSearch.setOnClickListener {
 
-            sharedViewModel.setString(binding.editTextSearch.text.toString())
+            if(AppUtils.validateField(binding.editTextSearch)){
+                AppUtils.hideKeyboard(requireContext(),binding.editTextSearch)
+                sharedViewModel.setString(binding.editTextSearch.text.toString())
+                findNavController().navigate(R.id.action_SearchFragment_to_FirstFragment)
+            }else{
+                binding.editTextSearch.requestFocus()
+                AppUtils.showKeyboard(requireContext(),binding.editTextSearch)
+            }
 
-            findNavController().navigate(R.id.action_SearchFragment_to_FirstFragment)
         }
     }
+
 }
---
+++ app/src/main/java/com/bryanollivie/appml/util/AppUtils.kt
@@ -0,0 +1,29 @@
+package com.bryanollivie.appml.util
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import dagger.hilt.android.internal.Contexts
+
+object AppUtils {
+
+    fun validateField(editText: EditText):Boolean {
+        return if (editText.text.toString().trim().isEmpty()) {
+            editText.error = "Por favor, insira algum texto!"
+            false
+        }else{
+            true
+        }
+    }
+
+    fun showKeyboard(context: Context, view: View) {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+    }
+    fun hideKeyboard(context: Context, view: View) {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+}
