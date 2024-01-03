7d05ccb620e79628f542ba3e8287ec14f8dce047
bryanollivie <bryanollivie@gmail.com>
2024-01-03 04:04:37 -0300

    screen error done

--- app/src/main/java/com/bryanollivie/appml/util/FragmentExtensions.kt
+++ app/src/main/java/com/bryanollivie/appml/util/FragmentExtensions.kt
@@ -1,10 +1,12 @@
 package com.bryanollivie.appml.util
 
 import android.opengl.Visibility
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.bryanollivie.appml.R
@@ -13,9 +15,17 @@ fun Fragment.showErrorLayout(msg: String, tryAgain: () -> Unit) {
     val viewErro = LayoutInflater.from(context).inflate(R.layout.error_layout, null)
     val textViewErro = viewErro.findViewById<TextView>(R.id.textViewErro)
     val buttonTentarNovamente = viewErro.findViewById<Button>(R.id.buttonTryAgain)
+    val layoutParams = LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.MATCH_PARENT,
+        LinearLayout.LayoutParams.MATCH_PARENT
+    )
+
+    layoutParams.gravity = Gravity.CENTER
+
 
     textViewErro.text = msg
     buttonTentarNovamente.setOnClickListener { tryAgain() }
+    viewErro.layoutParams = layoutParams
     viewErro.visibility = View.VISIBLE
 
     val container = view?.findViewById<ViewGroup>(com.google.android.material.R.id.container)
